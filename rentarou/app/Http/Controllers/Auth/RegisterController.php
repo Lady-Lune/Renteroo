fdc1fa100ad138314bde3107d13c0e7ed0870ff4
Nas <180202109+Lady-Lune@users.noreply.github.com>
2025-11-27 10:30:57 +0530

    final

--- rentarou/app/Http/Controllers/Auth/RegisterController.php
+++ rentarou/app/Http/Controllers/Auth/RegisterController.php
@@ -52,7 +52,7 @@ class RegisterController extends Controller
         'name' => ['required', 'string', 'max:255'],
         'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
         'password' => ['required', 'string', 'min:8', 'confirmed'],
-        'role' => ['required', 'in:admin,customer'], // ⭐ ADD THIS
+        'role' => [ 'in:admin,customer'], 
     ]);
 }
 
@@ -68,7 +68,7 @@ class RegisterController extends Controller
         'name' => $data['name'],
         'email' => $data['email'],
         'password' => Hash::make($data['password']),
-        'role' => $data['role'] ?? 'customer', // ⭐ ADD THIS
+        'role' => $data['role'] ?? 'customer',
     ]);
 }
 }
